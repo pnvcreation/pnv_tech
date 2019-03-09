72026dccc099f5eb560502d721b17c1992d5b3c3
pnv <vanjarapankaj.140180107059@gmail.com>
2019-03-09 16:19:20 +0530

    Version 1

--- intro.py
+++ intro.py
@@ -3,6 +3,17 @@
 # git 
     # one type of software
     # distributive version controlling system
+    # git free for open sorce code
+    # paid for private repository
+    # using git we back to over lower version
+        # If three friends(amit,mohan,pankaj) works in one project
+        # amit pull file throught git & chnages this after push in git
+        # after mohan pull file throught git & chnages this after push in git
+        # after pankaj pull file throught git & chnages this after pushin git
+        # if amit not satisfied this code 
+        # so, this see who have changed code using 'git log' command
+        # and if amit need four their version so, back this version
+        # this lower version save using commit 
 # github
     # code hosting service
     # github uses git version control    
@@ -13,3 +24,13 @@
         # know status
     # git init
         # made repositary of git
+    # git add .
+        # file stages
+        # file move working to staging area
+    # git commit -m "Version 1"
+        # what file change this give
+    # git config --global user.email "vanjarapankaj.140180107059@gmail.com"
+        # if commit not run so, give this
+    # git log
+        # changes all the developers
+    
