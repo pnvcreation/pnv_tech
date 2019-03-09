a139cb250da9398a33bc2fad15badf6c73209a49
unknown <vanjarapankaj.140180107059@gmail.com>
2019-03-09 12:05:18 +0530

    Version 1

---
+++ intro.py
@@ -0,0 +1,15 @@
+# manage of the files
+# if many programmer work in one project so, git throw we know what is changes
+# git 
+    # one type of software
+    # distributive version controlling system
+# github
+    # code hosting service
+    # github uses git version control    
+
+# codes for git
+    # git --version
+    # git status
+        # know status
+    # git init
+        # made repositary of git
